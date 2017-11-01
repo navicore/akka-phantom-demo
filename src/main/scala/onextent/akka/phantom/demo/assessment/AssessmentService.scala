fc9cd6b401ec4a219ed7fa79d2f9006ab19b43b0
Ed Sweeney <ed@onextent.com>
2017-11-01 08:06:02 -0700

    remove eventhubs

--- src/main/scala/onextent/akka/phantom/demo/assessment/AssessmentService.scala
+++ src/main/scala/onextent/akka/phantom/demo/assessment/AssessmentService.scala
@@ -2,13 +2,9 @@ package onextent.akka.phantom.demo.assessment
 
 import akka.actor._
 import akka.util.Timeout
-import com.microsoft.azure.reactiveeventhubs.ResumeOnError._
-import com.microsoft.azure.reactiveeventhubs.SourceOptions
-import com.microsoft.azure.reactiveeventhubs.scaladsl.EventHub
 import com.typesafe.scalalogging.LazyLogging
 import onextent.akka.phantom.demo.assessment.AssessmentService.Get
-import onextent.akka.phantom.demo.streams.db.{DbSink, Holder}
-import onextent.akka.phantom.demo.streams.utils.{Console, ExtractBodies}
+import onextent.akka.phantom.demo.streams.db.Holder
 
 object AssessmentService {
   def props(implicit timeout: Timeout) = Props(new AssessmentService)
@@ -20,14 +16,6 @@ object AssessmentService {
 class AssessmentService(implicit timeout: Timeout)
     extends Actor
     with LazyLogging {
-
-  EventHub()
-    .source(SourceOptions().fromSavedOffsets().saveOffsets())
-    .alsoTo(Console())
-    .via(ExtractBodies("assessment"))
-    .via(ExtractAssessments())
-    .to(DbSink[Assessment])
-    .run()
 
   override def receive: PartialFunction[Any, Unit] = {
     case Get(name) =>
